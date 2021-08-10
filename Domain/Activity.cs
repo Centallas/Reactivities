23bdbd892d61a4a517c9f31b3943f13aa62407d1
Edward <edward@bwired.ca>
2021-08-09 23:53:56 -0500

    End of section 10

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,5 +1,6 @@
 using System;
 
+
 namespace Domain
 {
     public class Activity
@@ -11,6 +12,6 @@ namespace Domain
         public string Category { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
-        
+
     }
 }
