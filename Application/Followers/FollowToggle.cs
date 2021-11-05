f5d095c437f4819fa4ef41aa9d6b9c996f9321ca
Edward <edward@bwired.ca>
2021-11-04 21:49:38 -0500

    End of section 20

---
+++ Application/Followers/FollowToggle.cs
@@ -0,0 +1,71 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Core;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Followers
+{
+    public class FollowToggle
+    {
+        public class Command : IRequest<Result<Unit>>
+        {
+            public string TargetUsername { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                _userAccessor = userAccessor;
+                _context = context;
+            }
+
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                //Get the users involve in this particular request
+                // this is the user we gonna use to follow the another user
+                var observer = await _context.Users.FirstOrDefaultAsync(x =>
+                    x.UserName == _userAccessor.GetUsername());
+
+                var target = await _context.Users.FirstOrDefaultAsync(x =>
+                    x.UserName == request.TargetUsername);
+
+                if (target == null) return null;
+
+                var following = await _context.UserFollowings.FindAsync(observer.Id, target.Id);
+
+                if (following == null)
+                {
+                    following = new UserFollowing
+                    {
+                        Observer = observer,
+                        Target = target
+                    };
+
+                    _context.UserFollowings.Add(following);
+                }
+                else
+                {
+                    //if we do have a following 
+                    _context.UserFollowings.Remove(following);
+                }
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Result<Unit>.Success(Unit.Value);
+
+                //if it fell return results
+
+                return Result<Unit>.Failure("Failed to update following");
+
+            }
+        }
+
+    }
+}
