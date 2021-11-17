252fb3c19ac48a10333dd3403beb4761f96e796a
Edward <edward@bwired.ca>
2021-11-17 14:10:39 -0500

    Fix Code Errors

--- API/Startup.cs
+++ API/Startup.cs
@@ -78,10 +78,10 @@ namespace API
             {
                 app.Use(async (context, next)=> 
                 {
-                   context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000")  
+                   context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");  
                    //to call the next piece of middleware after we add our Header
                    await next.Invoke();
-                })
+                });
             }
 
             //app.UseHttpsRedirection();
